a309e0d667b0397856a7eb32c520d233388e63a0
Lxxyx <Lxxyxzj@gmail.com>
2021-01-13 21:52:53 +0800

    cli: add routes command

--- packages/faas-cli-plugin-midway-hooks/src/__tests__/plugin.test.ts
+++ packages/faas-cli-plugin-midway-hooks/src/__tests__/plugin.test.ts
@@ -1,33 +1,13 @@
-import { CommandCore } from '@midwayjs/command-core'
-import { loadSpec } from '@midwayjs/serverless-spec-builder'
-import { PackagePlugin } from '@midwayjs/fcli-plugin-package'
-import { AliyunFCPlugin } from '@midwayjs/fcli-plugin-fc'
 import { resolve } from 'path'
-import { existsSync, remove } from 'fs-extra'
+import { existsSync } from 'fs-extra'
+import execa from 'execa'
 
 test('package', async () => {
+  const f = resolve(__dirname, '../../node_modules/.bin/f')
   const fixture = resolve(__dirname, './fixtures/package')
   const dist = resolve(fixture, './.serverless')
 
-  await remove(dist)
-
-  const core = new CommandCore({
-    config: {
-      servicePath: fixture,
-    },
-    commands: ['package'],
-    service: loadSpec(fixture),
-    provider: 'aliyun',
-    log: {
-      log: () => {},
-    },
-  })
-
-  core.addPlugin(PackagePlugin)
-  core.addPlugin(AliyunFCPlugin)
-
-  await core.ready()
-  await core.invoke(['package'])
+  await execa(f, ['package'], { cwd: fixture })
 
   expect(existsSync(resolve(dist, 'registerFunction.js'))).toBeTruthy()
   expect(existsSync(resolve(dist, 'index-controller.js'))).toBeTruthy()
